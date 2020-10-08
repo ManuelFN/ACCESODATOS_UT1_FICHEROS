7d31089b6fe9e3cb29a3ed67998d0cf7cde0470a
ManuelFN <55835494+ManuelFN@users.noreply.github.com>
2020-10-08 10:21:18 +0200

    2.0
    Crear, Propiedades, Borrar, Editar y mejora de la interfaz gráfica

--- TrabajoFichero_1/src/main.java
+++ TrabajoFichero_1/src/main.java
@@ -10,32 +10,68 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.FileTime;
+import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
 
         JFrame ventana = new JFrame("Trabajo Fichero");
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        ventana.setSize(300,200);
+        ventana.setSize(550,500);
         ventana.setResizable(false);
 
-        JPanel panelPrincipal = new JPanel();
-        panelPrincipal.setBorder(new EmptyBorder(30,60,0,60));
+        JPanel panelPrincipal = new JPanel(new GridLayout(3,1));
+        panelPrincipal.setBorder(new EmptyBorder(10,0,10,0));
+        JPanel panelTitulo = new JPanel();
+        panelTitulo.setBorder(new EmptyBorder(60,60,0,60));
+        JPanel panelCampo = new JPanel();
+        panelCampo.setBorder(new EmptyBorder(0,60,0,60));
+        JPanel panelBotones = new JPanel(new GridLayout(4,2));
+        panelBotones.setBorder(new EmptyBorder(30,60,0,60));
 
         JLabel titulo = new JLabel("Programa creador de ficheros");
-        titulo.setBorder(new EmptyBorder(0,0,30,0));
+        titulo.setFont(new Font("Arial", Font.BOLD, 30));
+        titulo.setBorder(new EmptyBorder(0,200,30,200));
+        JLabel etiInsert = new JLabel("Insertar contenido (opcional)");
+        etiInsert.setBorder(new EmptyBorder(20,0,10,0));
 
         JButton botonCrear = new JButton("Crear fichero");
-        JButton botonSeleccionar = new JButton("Seleccionar fichero");
+        JButton botonPropiedades = new JButton("Propiedades fichero");
+        JButton botonInsertar = new JButton("Insertar texto (opcional)");
+        JButton botonEditar = new JButton("Editar fichero");
+        JButton botonContarPalabras = new JButton("Contar palabras");
+        JButton botonContarVocales = new JButton("Contar vocales");
+        JButton botonCifrar = new JButton("Cifrar fichero");
+        JButton botonBorrarFich = new JButton("Borrar fichero");
 
-        panelPrincipal.add(titulo);
-        panelPrincipal.add(botonCrear);
-        panelPrincipal.add(botonSeleccionar);
+        JTextArea campoCont = new JTextArea();
+        campoCont.setPreferredSize(new Dimension(500,200));
+        campoCont.setLineWrap(true);
+        campoCont.setEditable(false);
+
+
+        panelTitulo.add(titulo);
+        panelCampo.add(campoCont);
+        panelBotones.add(botonCrear);
+        panelBotones.add(botonPropiedades);
+        panelBotones.add(botonInsertar);
+        panelBotones.add(botonEditar);
+        panelBotones.add(botonContarPalabras);
+        panelBotones.add(botonContarVocales);
+        panelBotones.add(botonCifrar);
+        panelBotones.add(botonBorrarFich);
+        ventana.add(panelTitulo);
+        ventana.add(panelCampo);
+        ventana.add(panelBotones);
+        panelPrincipal.add(panelTitulo);
+        panelPrincipal.add(panelCampo);
+        panelPrincipal.add(panelBotones);
         ventana.add(panelPrincipal);
 
         ventana.setVisible(true);
 
         JFileChooser ventanaFichero = new JFileChooser();
+        ventanaFichero.setCurrentDirectory(new File("C:\\Users\\User-01\\Desktop"));
 
         botonCrear.addActionListener(new ActionListener() {
             @Override
@@ -50,6 +86,8 @@ public class main {
                         File fichero = new File(rutaFich);
                         BufferedWriter escribir = new BufferedWriter(new FileWriter(nombreFich));
                         fichero.createNewFile();
+                        campoCont.setEditable(true);
+
                         escribir.close();
                     } catch (Exception o) {
                         o.printStackTrace();
@@ -58,7 +96,7 @@ public class main {
             }
         });
 
-        botonSeleccionar.addActionListener(new ActionListener() {
+        botonPropiedades.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
@@ -67,7 +105,7 @@ public class main {
                 if (seleccion == JFileChooser.APPROVE_OPTION) {
                     JFrame ventanaSeleccion = new JFrame("Insertar información");
                     ventanaSeleccion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                    ventanaSeleccion.setSize(300,400);
+                    ventanaSeleccion.setSize(300,320);
                     ventanaSeleccion.setResizable(false);
 
                     JPanel panelSeleccion = new JPanel();
@@ -80,13 +118,11 @@ public class main {
                     campoInsert.setEditable(false);
                     campoContenido.setEditable(false);
                     JButton botonMostrarCont = new JButton("Mostrar contenido");
-                    JButton botonBorrarFich = new JButton("Borrar fichero");
 
                     panelSeleccion.add(tituloPropFich);
                     panelSeleccion.add(campoInsert);
                     panelSeleccion.add(botonMostrarCont);
                     panelSeleccion.add(campoContenido);
-                    panelSeleccion.add(botonBorrarFich);
                     ventanaSeleccion.add(panelSeleccion);
 
                     ventanaSeleccion.setVisible(true);
@@ -107,18 +143,11 @@ public class main {
 
                                     String linea;
                                     while ((linea = leer.readLine()) != null) {
-                                        campoContenido.append(linea + "\n");
+                                        campoContenido.append(linea+ "\n");
                                         System.out.println(linea);
                                     }
 
                                     leer.close();
-
-                                    botonBorrarFich.addActionListener(new ActionListener() {
-                                        @Override
-                                        public void actionPerformed(ActionEvent e) {
-
-                                        }
-                                    });
 
                                 }catch (IOException u) {
                                     u.printStackTrace();
@@ -129,6 +158,44 @@ public class main {
                     } catch (IOException ioException) {
                         ioException.printStackTrace();
                     }
+                }
+            }
+        });
+
+        botonInsertar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    String frase = campoCont.getText();
+                    BufferedWriter escribir = new BufferedWriter(new FileWriter(ventanaFichero.getSelectedFile().getAbsolutePath()));
+                    escribir.write(frase);
+                    escribir.close();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+            }
+        });
+
+        botonBorrarFich.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    ventanaFichero.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                    int seleccion = ventanaFichero.showOpenDialog(ventanaFichero.getParent());
+
+                    if (seleccion == JFileChooser.APPROVE_OPTION) {
+                        File fichero = new File(ventanaFichero.getSelectedFile().getAbsolutePath());
+                        fichero.delete();
+                        if (fichero.exists()) {
+                            campoCont.setText("No se ha podido borrar el fichero...");
+                        } else {
+                            campoCont.setText("Fichero borrado con exito...");
+                        }
+
+                    }
+
+                } catch (Exception ex) {
+                    System.out.println("Ha habido problemas " + ex.getMessage());
                 }
             }
         });
